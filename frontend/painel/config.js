25381aba5f352b63e844d183ef00ede79dc39aaf
4Avalon <bitconha89@gmail.com>
2025-04-22 01:17:16 -0300

    Atualizações no projeto

--- frontend/painel/config.js
+++ frontend/painel/config.js
@@ -1,4 +1,4 @@
 // config.js
 export const BASE_URL = window.location.hostname.includes("localhost")
   ? "http://localhost:5000"
-  : "https://teu-backend.railway.app"; // substitua pelo seu real
+  : "https://amazonalign.up.railway.app"; // substitua pelo seu real
